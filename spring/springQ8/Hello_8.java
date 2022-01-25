1c1946c7e5fb1fd843bf1268bdb6cac8945084fc
Mukul200197 <78581572+Mukul200197@users.noreply.github.com>
2022-01-25 15:42:14 +0530

    Q8 Spring

---
+++ spring/springQ8/Hello_8.java
@@ -0,0 +1,15 @@
+
+public class Hello_9 {
+
+	public void init() throws Exception
+    {
+        System.out.println( " I'm "  + " the init() method");
+    }
+
+    public void destroy() throws Exception
+    {
+        System.out.println("I am " + " the destroy() method");
+    }
+	
+	
+}
